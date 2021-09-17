06a8cb2307e88c2a93d4240c595c5ddc537bcdf9
Joshua Shane Martin <josmar52789@gmail.com>
2021-09-17 13:46:37 -0400

    Updating URL for production.

--- Brizbee.Dashboard/Program.cs
+++ Brizbee.Dashboard/Program.cs
@@ -17,7 +17,7 @@ namespace Brizbee.Dashboard
             // Configure HttpClient to communicate with API.
             builder.Services.AddHttpClient<ApiService>(client =>
             {
-                client.BaseAddress = new Uri("http://localhost:54313");
+                client.BaseAddress = new Uri("https://app-brizbee-prod.azurewebsites.net");
             });
 
             // Configure additional services.
