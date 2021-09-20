7833bdd3b795da719936ac6018dffd06db67f1b3
Joshua Shane Martin <josmar52789@gmail.com>
2021-09-20 14:28:01 -0400

    Starting stylesheets.

--- Brizbee.Web/Controllers/ReportsController.cs
+++ Brizbee.Web/Controllers/ReportsController.cs
@@ -289,6 +289,78 @@ namespace Brizbee.Web.Controllers
             //if (!currentUser.CanViewReports)
             //    return StatusCode(HttpStatusCode.Forbidden);
 
+            var styleSheet = new Stylesheet(
+                new Fonts(
+                    new Font(                                                               // Index 0 - The default font.
+                        new FontSize() { Val = 11 },
+                        new Color() { Rgb = new HexBinaryValue() { Value = "00000000" } },
+                        new FontName() { Val = "Calibri" }),
+                    new Font(                                                               // Index 1 - The bold font.
+                        new Bold(),
+                        new FontSize() { Val = 11 },
+                        new Color() { Rgb = new HexBinaryValue() { Value = "00000000" } },
+                        new FontName() { Val = "Calibri" }),
+                    new Font(                                                               // Index 2 - The Italic font.
+                        new Italic(),
+                        new FontSize() { Val = 11 },
+                        new Color() { Rgb = new HexBinaryValue() { Value = "00000000" } },
+                        new FontName() { Val = "Calibri" }),
+                    new Font(                                                               // Index 2 - The Times Roman font. with 16 size
+                        new FontSize() { Val = 16 },
+                        new Color() { Rgb = new HexBinaryValue() { Value = "00000000" } },
+                        new FontName() { Val = "Times New Roman" })
+                ),
+                new Fills(
+                    new Fill(                                                           // Index 0 - The default fill.
+                        new PatternFill() { PatternType = PatternValues.None }),
+                    new Fill(                                                           // Index 1 - The default fill of gray 125 (required)
+                        new PatternFill() { PatternType = PatternValues.Gray125 }),
+                    new Fill(                                                           // Index 2 - The yellow fill.
+                        new PatternFill(
+                            new ForegroundColor() { Rgb = new HexBinaryValue() { Value = "FFFFFF00" } }
+                        )
+                        { PatternType = PatternValues.Solid })
+                ),
+                new Borders(
+                    new Border(                                                         // Index 0 - The default border.
+                        new LeftBorder(),
+                        new RightBorder(),
+                        new TopBorder(),
+                        new BottomBorder(),
+                        new DiagonalBorder()),
+                    new Border(                                                         // Index 1 - Applies a Left, Right, Top, Bottom border to a cell
+                        new LeftBorder(
+                            new Color() { Auto = true }
+                        )
+                        { Style = BorderStyleValues.Thin },
+                        new RightBorder(
+                            new Color() { Auto = true }
+                        )
+                        { Style = BorderStyleValues.Thin },
+                        new TopBorder(
+                            new Color() { Auto = true }
+                        )
+                        { Style = BorderStyleValues.Thin },
+                        new BottomBorder(
+                            new Color() { Auto = true }
+                        )
+                        { Style = BorderStyleValues.Thin },
+                        new DiagonalBorder())
+                ),
+                new CellFormats(
+                    new CellFormat() { FontId = 0, FillId = 0, BorderId = 0 },                          // Index 0 - The default cell style.  If a cell does not have a style index applied it will use this style combination instead
+                    new CellFormat() { FontId = 1, FillId = 0, BorderId = 0, ApplyFont = true },       // Index 1 - Bold 
+                    new CellFormat() { FontId = 2, FillId = 0, BorderId = 0, ApplyFont = true },       // Index 2 - Italic
+                    new CellFormat() { FontId = 3, FillId = 0, BorderId = 0, ApplyFont = true },       // Index 3 - Times Roman
+                    new CellFormat() { FontId = 0, FillId = 2, BorderId = 0, ApplyFill = true },       // Index 4 - Yellow Fill
+                    new CellFormat(                                                                   // Index 5 - Alignment
+                        new Alignment() { Horizontal = HorizontalAlignmentValues.Center, Vertical = VerticalAlignmentValues.Center }
+                    )
+                    { FontId = 0, FillId = 0, BorderId = 0, ApplyAlignment = true },
+                    new CellFormat() { FontId = 0, FillId = 0, BorderId = 1, ApplyBorder = true }      // Index 6 - Border
+                )
+            );
+
             using (var stream = new MemoryStream())
             using (var document = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook))
             {
@@ -298,6 +370,14 @@ namespace Brizbee.Web.Controllers
 
                 var workbookpart = document.AddWorkbookPart();
                 workbookpart.Workbook = new Workbook();
+
+
+                // ------------------------------------------------------------
+                // Apply stylesheet.
+                // ------------------------------------------------------------
+
+                var workStylePart = workbookpart.AddNewPart<WorkbookStylesPart>();
+                workStylePart.Stylesheet = styleSheet;
 
 
                 // ------------------------------------------------------------
@@ -320,7 +400,30 @@ namespace Brizbee.Web.Controllers
                 // ------------------------------------------------------------
 
                 var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                var sheetProperties = new SheetProperties(new PageSetupProperties());
                 worksheetPart.Worksheet = new Worksheet(new SheetData());
+                worksheetPart.Worksheet.SheetProperties = sheetProperties;
+
+
+
+
+                // Set the FitToPage property to true
+                //worksheetPart.Worksheet.SheetProperties.PageSetupProperties.FitToPage = BooleanValue.FromBoolean(true);
+
+                var pgOr = new PageSetup();
+                pgOr.Orientation = OrientationValues.Landscape;
+                worksheetPart.Worksheet.AppendChild(pgOr);
+
+
+
+
+
+
+
+
+
+
+
 
 
                 // ------------------------------------------------------------
@@ -355,12 +458,28 @@ namespace Brizbee.Web.Controllers
                 var rowIndex = (uint)1;
                 foreach (var user in users)
                 {
-                    var sheetRow = worksheetPart.Worksheet.Elements<Row>().ElementAt((int)rowIndex - 1);
+                    var rowBreak = new Break() { Id = rowIndex, Max = 16383u, ManualPageBreak = true };
+
+                    var rowBreaks = worksheetPart.Worksheet.GetFirstChild<RowBreaks>();
+
+                    if (rowBreaks == null)
+                    {
+                        rowBreaks = new RowBreaks();
+                        rowBreaks.ManualBreakCount = 0u;
+                        rowBreaks.Count = 0u;
+
+                        worksheetPart.Worksheet.Append(rowBreaks);
+                    }
+
+                    rowBreaks.Append(rowBreak);
+                    rowBreaks.ManualBreakCount++;
+                    rowBreaks.Count++;
 
                     // Set the value of the cell.
                     var cellA1 = InsertCellInWorksheet("A", rowIndex, worksheetPart);
                     cellA1.CellValue = new CellValue(user.Name);
                     cellA1.DataType = new EnumValue<CellValues>(CellValues.String);
+                    cellA1.StyleIndex = 4u;
 
                     rowIndex++;
                 }
