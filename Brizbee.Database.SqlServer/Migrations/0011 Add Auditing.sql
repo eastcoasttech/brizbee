0af1b2bd58b1bf40979ac8e42f9a7347ae0bc613
Joshua Shane Martin <josmar52789@gmail.com>
2021-10-06 14:06:17 -0400

    Adjusting formatting style.

--- Brizbee.Database.SqlServer/Migrations/0011 Add Auditing.sql
+++ Brizbee.Database.SqlServer/Migrations/0011 Add Auditing.sql
@@ -6,40 +6,42 @@ FOR [CanViewAudits];
 
 -- Punch Audits Table
 CREATE TABLE [dbo].[PunchAudits] (
-	[Id] BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_PunchAudits_Id] PRIMARY KEY
-	,[CreatedAt] DATETIME2(7) NOT NULL
-	,[OrganizationId] INT NOT NULL
-	,[UserId] INT NOT NULL
-	,[ObjectId] BIGINT NOT NULL
-	,[Action] VARCHAR(10) NOT NULL
-	,[Before] NVARCHAR(MAX) CONSTRAINT [CK_PunchAudits_Before] CHECK (ISJSON([Before]) = 1) NULL
-	,[After] NVARCHAR(MAX) CONSTRAINT [CK_PunchAudits_After] CHECK (ISJSON([After]) = 1) NULL
-	);
+    [Id] BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_PunchAudits_Id] PRIMARY KEY,
+    [CreatedAt] DATETIME2(7) NOT NULL,
+    [OrganizationId] INT NOT NULL,
+    [UserId] INT NOT NULL,
+    [ObjectId] BIGINT NOT NULL,
+    [Action] VARCHAR(10) NOT NULL,
+    [Before] NVARCHAR(MAX) CONSTRAINT [CK_PunchAudits_Before] CHECK (ISJSON([Before]) = 1) NULL,
+    [After] NVARCHAR(MAX) CONSTRAINT [CK_PunchAudits_After] CHECK (ISJSON([After]) = 1) NULL
+    );
 GO
 
+-- Indexes
 CREATE NONCLUSTERED INDEX [IX_OrganizationIdCreatedAtUserId] ON [dbo].[PunchAudits] (
-	[OrganizationId]
-	,[CreatedAt]
-	,[UserId]
-	)
+    [OrganizationId],
+    [CreatedAt],
+    [UserId]
+    )
 GO
 
 -- Time Card Audits Table
 CREATE TABLE [dbo].[TimeCardAudits] (
-	[Id] BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_TimeCardAudits_Id] PRIMARY KEY
-	,[CreatedAt] DATETIME2(7) NOT NULL
-	,[OrganizationId] INT NOT NULL
-	,[UserId] INT NOT NULL
-	,[ObjectId] BIGINT NOT NULL
-	,[Action] VARCHAR(10) NOT NULL
-	,[Before] NVARCHAR(MAX) CONSTRAINT [CK_TimeCardAudits_Before] CHECK (ISJSON([Before]) = 1) NULL
-	,[After] NVARCHAR(MAX) CONSTRAINT [CK_TimeCardAudits_After] CHECK (ISJSON([After]) = 1) NULL
-	);
+    [Id] BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_TimeCardAudits_Id] PRIMARY KEY,
+    [CreatedAt] DATETIME2(7) NOT NULL,
+    [OrganizationId] INT NOT NULL,
+    [UserId] INT NOT NULL,
+    [ObjectId] BIGINT NOT NULL,
+    [Action] VARCHAR(10) NOT NULL,
+    [Before] NVARCHAR(MAX) CONSTRAINT [CK_TimeCardAudits_Before] CHECK (ISJSON([Before]) = 1) NULL,
+    [After] NVARCHAR(MAX) CONSTRAINT [CK_TimeCardAudits_After] CHECK (ISJSON([After]) = 1) NULL
+    );
 GO
 
+-- Indexes
 CREATE NONCLUSTERED INDEX [IX_OrganizationIdCreatedAtUserId] ON [dbo].[TimeCardAudits] (
-	[OrganizationId]
-	,[CreatedAt]
-	,[UserId]
-	)
+    [OrganizationId],
+    [CreatedAt],
+    [UserId]
+    )
 GO
