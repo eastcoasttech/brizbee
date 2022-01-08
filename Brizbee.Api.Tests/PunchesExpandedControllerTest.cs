ef05c60e3a472d4f21103d82669123473237783a
Joshua Shane Martin <josmar52789@gmail.com>
2022-01-08 18:56:39 -0500

    Adding tests for punch and jobs expanded controllers.

---
+++ Brizbee.Api.Tests/PunchesExpandedControllerTest.cs
@@ -0,0 +1,144 @@
+﻿//
+//  PunchesExpandedControllerTest.cs
+//  BRIZBEE API
+//
+//  Copyright (C) 2019-2021 East Coast Technology Services, LLC
+//
+//  This file is part of the BRIZBEE API.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace Brizbee.Api.Tests
+{
+    [TestClass]
+    public class PunchesExpandedControllerTest
+    {
+        public IConfiguration _configuration { get; set; }
+
+        public SqlContext _context { get; set; }
+
+        Helper helper = new Helper();
+
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public PunchesExpandedControllerTest()
+        {
+            // Setup configuration
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            _configuration = configurationBuilder.Build();
+
+            // Setup database context
+            var options = new DbContextOptionsBuilder<SqlContext>()
+                .UseSqlServer(_configuration["ConnectionStrings:SqlContext"])
+                .Options;
+            _context = new SqlContext(options);
+        }
+
+        [TestInitialize]
+        public void PrepareForTest()
+        {
+            helper.Prepare();
+        }
+
+        [TestCleanup]
+        public void CleanupAfterTest()
+        {
+            helper.Cleanup();
+        }
+
+        [TestMethod]
+        public async System.Threading.Tasks.Task GetAllPunches_Should_ReturnSuccessfully()
+        {
+            // ----------------------------------------------------------------
+            // Arrange
+            // ----------------------------------------------------------------
+
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
+                    {
+                        configurationBuilder.AddJsonFile("appsettings.json");
+                    });
+                });
+
+            var client = application.CreateClient();
+
+            // User will be authenticated
+            var currentUser = _context.Users
+                .Where(u => u.EmailAddress == "test.user.a@brizbee.com")
+                .FirstOrDefault();
+
+            var token = GenerateJSONWebToken(currentUser!.Id, currentUser!.EmailAddress!);
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+
+            // ----------------------------------------------------------------
+            // Act
+            // ----------------------------------------------------------------
+
+            var min = new DateTime(2022, 1, 1).ToString("yyyy-MM-ddTHH:mm:ssZ");
+            var max = new DateTime(2022, 1, 2).ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+            var response = await client.GetAsync($"api/PunchesExpanded?min={min}&max={max}");
+
+
+            // ----------------------------------------------------------------
+            // Assert
+            // ----------------------------------------------------------------
+
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        }
+
+        private string GenerateJSONWebToken(int userId, string emailAddress)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, emailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
