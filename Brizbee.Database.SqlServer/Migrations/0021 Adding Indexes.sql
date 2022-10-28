f18c1aaa51457effd7dfb63cf6c01556d8247771
Joshua Shane Martin <josmar52789@gmail.com>
2022-10-28 16:23:21 -0400

    Adding indexes for performance.

---
+++ Brizbee.Database.SqlServer/Migrations/0021 Adding Indexes.sql
@@ -0,0 +1,14 @@
+﻿CREATE NONCLUSTERED INDEX [IX_Punches_TaskId]
+    ON [dbo].[Punches] ([TaskId]);
+
+CREATE NONCLUSTERED INDEX [IX_Tasks_BaseServiceRateId]
+    ON [dbo].[Tasks] ([BaseServiceRateId]);
+    
+CREATE NONCLUSTERED INDEX [IX_Tasks_BasePayrollRateId]
+    ON [dbo].[Tasks] ([BasePayrollRateId]);
+    
+CREATE NONCLUSTERED INDEX [IX_Rates_ParentRateId]
+    ON [dbo].[Rates] ([ParentRateId]);
+    
+CREATE NONCLUSTERED INDEX [IX_Rates_OrganizationId]
+    ON [dbo].[Rates] ([OrganizationId]);
