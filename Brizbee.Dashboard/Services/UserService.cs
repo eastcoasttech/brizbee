b635446475bd85437f30c7c7c1f4efec11845c90
Joshua Shane Martin <josmar52789@gmail.com>
2024-09-22 21:16:30 -0400

    fix: do not show inactive users in list

--- Brizbee.Dashboard/Services/UserService.cs
+++ Brizbee.Dashboard/Services/UserService.cs
@@ -60,7 +60,7 @@ namespace Brizbee.Dashboard.Services
 
         public async Task<List<User>> SearchUsersAsync(string query)
         {
-            var response = await _apiService.GetHttpClient().GetAsync($"odata/Users?$filter=contains(Name,'{query}') and IsActive eq false&$select=EmailAddress,Name,Id");
+            var response = await _apiService.GetHttpClient().GetAsync($"odata/Users?$filter=contains(Name,'{query}') and IsActive eq true&$select=EmailAddress,Name,Id");
             response.EnsureSuccessStatusCode();
 
             using var responseContent = await response.Content.ReadAsStreamAsync();
