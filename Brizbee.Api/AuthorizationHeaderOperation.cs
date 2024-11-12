5651026dbdaf1a354a69cbc27b9168adaa2bfbb6
Joshua Shane Martin <josmar52789@gmail.com>
2024-11-12 16:42:51 -0500

    refactor: adding comments for license, implement suggested formatting

--- Brizbee.Api/AuthorizationHeaderOperation.cs
+++ Brizbee.Api/AuthorizationHeaderOperation.cs
@@ -1,26 +1,46 @@
-﻿using Microsoft.OpenApi.Models;
+﻿//
+//  AuthorizationHeaderOperation.cs
+//  BRIZBEE API
+//
+//  Copyright (C) 2019-2024 East Coast Technology Services, LLC
+//
+//  This file is part of the BRIZBEE API.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace Brizbee.Api
+namespace Brizbee.Api;
+
+public abstract class AuthorizationHeaderOperation : IOperationFilter
 {
-    public class AuthorizationHeaderOperation : IOperationFilter
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        // Validate the operation.
+        ArgumentNullException.ThrowIfNull(operation);
+
+        operation.Parameters ??= new List<OpenApiParameter>();
+
+        // Configure the Authorization header.
+        operation.Parameters.Add(new OpenApiParameter()
         {
-            // Validate operation
-            if (operation == null) { throw new ArgumentNullException(nameof(operation)); }
-
-            if (operation.Parameters == null)
-                operation.Parameters = new List<OpenApiParameter>();
-
-            // Authorization header
-            operation.Parameters.Add(new OpenApiParameter()
-            {
-                Name = "Authorization",
-                In = ParameterLocation.Header,
-                Description = "JWT",
-                Required = false
-            });
-        }
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Description = "JWT",
+            Required = false
+        });
     }
 }
