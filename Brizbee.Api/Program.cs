352646c1686ce5308bad15aea2d160cbff09ba55
Joshua Shane Martin <josmar52789@gmail.com>
2026-04-04 13:02:05 -0400

    refactor: specify data protection keys path

--- Brizbee.Api/Program.cs
+++ Brizbee.Api/Program.cs
@@ -23,6 +23,9 @@
 using Brizbee.Api;
 using Invio.Extensions.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +36,23 @@ using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string dataProtectionKeysDirectoryPath = builder.Configuration.GetValue<string>("DataProtectionKeysDirectoryPath") ??
+                                          throw new InvalidOperationException(
+                                              "'DataProtectionKeysDirectoryPath' must be provided.");
+
+if (string.IsNullOrEmpty(dataProtectionKeysDirectoryPath))
+{
+    throw new ArgumentNullException(nameof(dataProtectionKeysDirectoryPath));
+}
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysDirectoryPath))
+    .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration()
+    {
+        EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+        ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+    });
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
@@ -58,7 +78,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
                     };
                 })
-                
+
                 // Allow passing the token via query parameter, instead of a header.
                 .AddJwtBearerQueryStringAuthentication(options =>
                 {
